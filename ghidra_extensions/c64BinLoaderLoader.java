0a52029c9ed5a8262fd48b92a4f22fe6edb68761
David Youd <cryptoboy@gmail.com>
2022-12-10 19:10:15 -0800

    additional options validation in loader

--- ghidra_extensions/c64BinLoaderLoader.java
+++ ghidra_extensions/c64BinLoaderLoader.java
@@ -16,10 +16,10 @@
 package c64binloader;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.io.InputStream;
 
 import ghidra.app.util.HexLong;
 import ghidra.app.util.Option;
@@ -31,6 +31,7 @@ import ghidra.app.util.opinion.Loader;
 import ghidra.app.util.opinion.LoaderTier;
 import ghidra.framework.model.DomainObject;
 import ghidra.program.flatapi.FlatProgramAPI;
+import ghidra.program.model.address.Address;
 import ghidra.program.model.lang.Language;
 import ghidra.program.model.lang.LanguageCompilerSpecPair;
 import ghidra.program.model.listing.Program;
@@ -38,20 +39,10 @@ import ghidra.program.model.mem.Memory;
 import ghidra.program.model.mem.MemoryBlock;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
-import ghidra.program.model.address.Address;
-
-/*
- * Notes:
- * - https://github.com/NationalSecurityAgency/ghidra/blob/a64afa37a7873fefad84c7b994a50acf02beb062/Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/AbstractProgramLoader.java 
- * 
- * TODO:
- * - 
- * 
- */
 
 public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
-
-	public static final String VERSION = "v0.1.1";
+	// https://github.com/NationalSecurityAgency/ghidra/blob/a64afa37a7873fefad84c7b994a50acf02beb062/Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/AbstractProgramLoader.java
+	public static final String VERSION = "v0.1.2";
 	
 	public static final String OPTION_LOAD_FIRST_TWO_BYTES = "Load first two bytes?";
 	public static final String OPTION_LOAD_ADDRESS = "Load address";
@@ -97,49 +88,37 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 			Program program, TaskMonitor monitor, MessageLog log)
 			throws CancelledException, IOException {
 
-		// So many loader examples online that use monitor.setMessage(string) to provide feedback
-		// however, I can't tell where this text gets displayed in the GUI, if at all.
-		// To see feedback in the Import's Additional Information section after a load, I'm
-		// using log.appendMsg(string) instead.
-		// Note, neither log nor monitor strings show up in
-		//    C:\Users\USERNAME\.ghidra\.ghidra_10.2.2_PUBLIC\application.log
 		log.appendMsg(getName() + " (" + VERSION + ") loading \"" + provider.getFile().getName() + "\"");
 
-		Long loadAddr = null;
+		String loadAddrStr, entryAddrStr;
 		Long entry = null;
 		Boolean createStack = null;
+		Long loadAddr = 0L;		
 		int binContentStart = 2; // content normally starts two bytes in
 		
-		for (Option option : options) {
-			String optName = option.getName();
-			if (optName.equals(OPTION_LOAD_FIRST_TWO_BYTES)) {
-				if ((Boolean)option.getValue()) {
-					binContentStart = 0;
-				}
-			}
-			else if (optName.equals(OPTION_LOAD_ADDRESS)) {
-				loadAddr = Long.parseLong((String)option.getValue(), 16);
-			}
-			else if (optName.equals(OPTION_ENTRY_ADDRESS)) {
-				String tmp = (String)option.getValue();
-				if (tmp != null && tmp.length() > 0) {
-					entry = Long.parseLong(tmp, 16);
-				}
-			}
-			else if (optName.equals(OPTION_CREATE_STACK)) {
-				createStack = (Boolean)option.getValue();				
-			}			
+		// get options
+		if ((Boolean)getValForNamedOption(options, OPTION_LOAD_FIRST_TWO_BYTES)) {
+			binContentStart = 0;			
 		}
+		
+		loadAddrStr = (String)getValForNamedOption(options, OPTION_LOAD_ADDRESS);
+		if (loadAddrStr.length() > 0) {
+			loadAddr = Long.parseLong(loadAddrStr, 16);	
+		}
+		
+		entryAddrStr = (String)getValForNamedOption(options, OPTION_ENTRY_ADDRESS);
+		if (entryAddrStr != null && entryAddrStr.length() > 0) {
+			entry = Long.parseLong(entryAddrStr, 16);		
+		}
+		
+		createStack = (Boolean)getValForNamedOption(options, OPTION_CREATE_STACK);
 
+		// perform load
 		Memory mem = program.getMemory();
 		FlatProgramAPI api = new FlatProgramAPI(program);
-
-		// "if true, the block will be created as an OVERLAY which means that a new overlay address
-		// space will be created and the block will have a starting address at the same offset as
-		// the given start address parameter, but in the new address space."
-		boolean overlay = false;
+		boolean overlay = false; // true uses new address apce
 		MemoryBlock mb;
-		InputStream bytes = provider.getInputStream(binContentStart); // TODO: Right base class here?
+		InputStream bytes = provider.getInputStream(binContentStart);
 		long bytesLen = provider.length() - binContentStart;
 		String blockName = provider.getFile().getName();
 		Address loadAddress = api.toAddr(loadAddr);
@@ -171,13 +150,14 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 		// Mapped I/O			
 		try {
 			api.createLabel(api.toAddr(0x0000), "6510DDR", true);
-			api.createLabel(api.toAddr(0x0001), "BANKING_CASSETTE", true);	
+			api.createLabel(api.toAddr(0x0001), "BANKS_TAPE", true);	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
+	// full permissions on block
 	private void anythingGoes(MemoryBlock mb) {
 		mb.setRead(true);
 		mb.setWrite(true);
@@ -188,8 +168,7 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 	protected void createDefaultMemoryBlocks(Program program, Language language, MessageLog log) {
 		// The 6502.pspec has default memory blocks that I don't want; these are
 		// ZERO_PAGE and STACK (without execute priv).  This override keeps that from
-		// happening.  In the load(), I'll recreate STACK (can't just add execute to it, because
-		// default memory blocks don't yet exist at load() time).
+		// happening.  In the load(), recreate STACK.
 		return;
 	}
 
@@ -213,14 +192,14 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		String binaryStartAddrHex = Integer.toHexString(binaryStartAddr);
+		String binaryStartAddrHexStr = Integer.toHexString(binaryStartAddr);
 		
 		List<Option> list = new ArrayList<Option>();
 
         list.add(new Option(OPTION_LOAD_FIRST_TWO_BYTES, false, Boolean.class, 
             Loader.COMMAND_LINE_ARG_PREFIX + "-loadFirstTwoBytes"));         
 
-		list.add(new Option(OPTION_LOAD_ADDRESS, binaryStartAddrHex, String.class,
+		list.add(new Option(OPTION_LOAD_ADDRESS, binaryStartAddrHexStr, String.class,
 			Loader.COMMAND_LINE_ARG_PREFIX + "-loadAddr"));
 
 		list.add(new Option(OPTION_ENTRY_ADDRESS, "", String.class,
@@ -233,44 +212,25 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 	}
 	
 	@Override
-	// return null if all Options are valid or an error message String
+	// return null if all Options are valid or a String with error message
 	public String validateOptions(ByteProvider provider, LoadSpec loadSpec, List<Option> options, Program program) {
 
-		Boolean loadFirstTwoBytes = null;
+		Boolean loadFirstTwoBytes, createStack; 
 		HexLong loadAddr = null;
 		HexLong entry = null;
-		String optName, tmpString;	
+		String loadAddrStr, entryStr;	
+		long loadStart, loadEnd;
+		long codeLen = 0;
 
-		// gather input values and validate (in a horrible, order-independent way)
-		for (Option option : options) {
-			optName = option.getName();
-			if (optName.equals(OPTION_LOAD_FIRST_TWO_BYTES)) {
-				loadFirstTwoBytes = (Boolean)option.getValue();
-			}
-			else if (optName.equals(OPTION_LOAD_ADDRESS)) {
-				tmpString = (String)option.getValue();
-				if (!tmpString.equals("")) {
-					loadAddr = getHexValForHexString(tmpString);
-					if (loadAddr == null) {
-						return "\"" + tmpString + "\" is not valid hex";
-					}
-				}
-				if (loadAddr == null) {
-					loadAddr = new HexLong(0);
-				}
-			}
-			else if (optName.equals(OPTION_ENTRY_ADDRESS)){
-				tmpString = (String)option.getValue();
-				if (!tmpString.equals("")) {
-					entry = getHexValForHexString(tmpString);
-					if (entry == null) {
-						return "\"" + tmpString + "\" is not valid hex";
-					}
-				}
-			}
+		try {
+			loadFirstTwoBytes = (Boolean)getValForNamedOption(options, OPTION_LOAD_FIRST_TWO_BYTES);
+			loadAddrStr = ((String)getValForNamedOption(options, OPTION_LOAD_ADDRESS)).trim();					
+			entryStr = ((String)getValForNamedOption(options, OPTION_ENTRY_ADDRESS)).trim();
+			createStack = (Boolean)getValForNamedOption(options, OPTION_CREATE_STACK);			
+		} catch (IOException e) {
+			return "Error: cannot retreive option";
 		}
 
-		long codeLen = 0;
 		try {
 			codeLen = provider.length();
 		} catch (IOException e) {
@@ -279,29 +239,63 @@ public class c64BinLoaderLoader extends AbstractProgramWrapperLoader {
 		if (!loadFirstTwoBytes) {
 			codeLen -= 2;
 		}
-		
-		// additional work after all values gathered
-		for (Option option : options) {
-			optName = option.getName();		
 
-			if (optName.equals(OPTION_LOAD_ADDRESS)) {
-				long maxAddr = 0xffff - codeLen;
-				if (loadAddr.longValue() < 0 || loadAddr.longValue() > maxAddr) {
-					return "load addr range $0000 to $" + Long.toHexString(maxAddr);
-				}
+		// validate load address
+		if (loadAddrStr.length() == 0) {
+			loadAddr = new HexLong(0);
+		} else {
+			loadAddr = getHexValForHexString(loadAddrStr);
+			if (loadAddr == null) {
+				return "\"" + loadAddrStr + "\" is not valid hex";
+			}			
+		}
+		long maxLoadAddr = 0xffff - codeLen;
+		if (loadAddr.longValue() < 0 || loadAddr.longValue() > maxLoadAddr) {
+			return "load addr range $0000 to $" + Long.toHexString(maxLoadAddr);
+		}		
+
+		loadStart = loadAddr.longValue();  // inclusive
+		loadEnd = loadStart + codeLen;     // exclusive
+		
+		// validate entry address
+		if (entryStr.length() > 0) {
+			entry = getHexValForHexString(entryStr);
+			if (entry == null) {
+				return "\"" + entryStr + "\" is not valid hex";
 			}
-			else if (optName.equals(OPTION_ENTRY_ADDRESS)) {
-				if (entry != null) {
-					long lowEntry = loadAddr.longValue();
-					long highEntry = loadAddr.longValue() + codeLen;
-					if (entry.longValue() < lowEntry || entry.longValue() > highEntry) {
-						return "entry addr range $" + Long.toHexString(lowEntry) + " to $" + Long.toHexString(highEntry);
-					}
-				}
+			if (entry.longValue() < loadStart || entry.longValue() > loadEnd) {
+				return "entry addr range $" + Long.toHexString(loadStart) + " to $" + Long.toHexString(loadEnd);
 			}
 		}
 
+		// validate room for stack block
+		if (createStack) {
+			if ((loadStart <= 0x1ff) && (loadEnd > 0x100)) {
+				return "stack block would overlap with loaded code block";
+			}
+		}	
+		
 		return null; // null here means success
+	}
+
+	protected Object getValForNamedOption(List<Option> options, String optionName) throws IOException {
+		// https://ghidra.re/ghidra_docs/api/ghidra/app/util/Option.html		
+		for (Option option : options) {
+			if (option.getName().equals(optionName)) {
+				return option.getValue();
+			}
+		}
+		throw new IOException("Error: option not found");
+	}
+	
+	protected void setValForNamedOption(List<Option> options, String optionName, Object value) throws IOException {
+		for (Option option : options) {
+			if (option.getName().equals(optionName)) {	
+				option.setValue(value);
+				return;
+			}
+		}
+		throw new IOException("Error: option not found");		
 	}
 
 	// return a HexLong instance, or null if string is not valid hex
